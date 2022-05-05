d6d0b8fc0f001afd20277d42e528f6bf49c9271a
pauladc <pauladc@mit.edu>
2022-05-05 03:18:54 -0400

    added rules

--- lab10/lab.py
+++ lab10/lab.py
@@ -1,8 +1,6 @@
 """6.009 Lab 10: Snek Is You Video Game"""
 
-from curses.ascii import isupper
 import doctest
-from xxlimited import new
 
 # NO ADDITIONAL IMPORTS!
 
@@ -20,40 +18,104 @@ direction_vector = {
     "right": (0, +1),
 }
 
-def inside_board(original, direction, board):
-    if original[0] + direction[0] >= len(board) or original[1] + direction[1] >= len(board[0]):
+def inside_board(original, len_board, direction = (0,0)):
+    if original[0] + direction[0] >= len_board or original[1] + direction[1] >= len_board:
         return False
     elif original[0] + direction[0] < 0 or original[1] + direction[1] < 0:
         return False
     else:
         return True
 
-def change_position(original, direction, board):
-    if inside_board(original, direction, board):
+def change_position(original, len_board, direction):
+    if inside_board(original, len_board, direction,):
         return (original[0] + direction[0], original[1] + direction[1])
     else:
         raise ValueError
 
+def get_obj(board, coordinates):
+    if inside_board(coordinates, board):
+        return board[coordinates[0]][coordinates[1]]
+    else:
+        return []
+
 
 class gameObj():
     def __init__(self, value, board, position):
+        self.properties = set()
         self.value = value
         self.board = board
         self.position = position
-        self.properties = set()
     
-    # def try_move(self, direction):
-    #     try_position = change_position(self.position, direction)
-    #     forward_neighbors = get_position(self.board, try_position)
-    #     if self.can_move is not None:
-    #         pass
-    #     elif any(map(lambda n: 'STOP' in n.properties and 'PUSH' not in n.properties, forward_neighbors)):
-    #         self.can_move = False
-    #     else:
-    #         self.can_move = all(map(lambda n: n.try_move(direction),
-    #                 filter(lambda n: n.properties & {'PUSH'}, forward_neighbors))) and \
-    #             position_in_bounds(self.board, try_position)
-    #     return self.can_move
+    def reset(self):
+        self.move_allowed = None # or True or False
+        self.moved = None # or True or False
+
+    def move_obj(self, direction):
+        try:
+            try_position = change_position(self.position, len(self.board), direction)
+        except:
+            print('in exception')
+            return self.board
+        self.board[self.position[0]][self.position[1]] = []
+        self.position = try_position
+        self.board[try_position[0]][try_position[1]].append(self)
+
+        
+def find_rules(game):
+    def search_neighbors(obj, nouns, len_board, properties_dic):
+        for direction in [(1, 0), (0, 1)]:
+            try:
+                new_position = change_position(obj.position, len_board, direction)
+            except:
+                continue
+            if obj.board[new_position[0]][new_position[1]] != []:
+                if obj.board[new_position[0]][new_position[1]][0].value == 'IS':
+                    try:
+                        new_position = change_position(new_position, len_board, direction)
+                        if obj.board[new_position[0]][new_position[1]] != []:
+                            if obj.board[new_position[0]][new_position[1]][0].value in PROPERTIES:
+                                obj.properties.add(obj.board[new_position[0]][new_position[1]][0].value)
+                                properties_dic[obj.board[new_position[0]][new_position[1]][0].value].append(obj)
+                                nouns[obj.value.lower()].append(obj.board[new_position[0]][new_position[1]][0].value)
+                            elif obj.board[new_position[0]][new_position[1]][0].value in NOUNS:
+                                nouns[obj.value.lower()].append(obj.board[new_position[0]][new_position[1]][0].value)
+                    except:
+                        pass    
+
+    objects, len_board, properties_dic = game['objects'], len(game['board']), game['properties']
+    nouns = {val.lower():[] for val in NOUNS}
+
+    for obj in objects:
+        if obj.value.isupper():
+            if obj.value in PROPERTIES:
+                obj.properties.add(obj.value)
+                properties_dic[obj.value].append(obj)
+            elif obj.value in NOUNS:
+                search_neighbors(obj, nouns, len_board, properties_dic)
+
+    for obj in objects:
+        if obj.value in nouns:
+            for props in nouns[obj.value]:
+                obj.properties.add(props)
+        elif obj.value in NOUNS or obj.value in PROPERTIES:
+            obj.properties = {}
+    print(nouns)
+
+    return nouns
+
+
+def are_nouns(game, nouns):
+    for obj in game['objects']:
+        if obj.value in nouns:
+            print('in if')
+            for props in nouns[obj.value]:
+                print('props are ', props)
+                if props in NOUNS:
+                    obj.value = props.lower()
+                    obj.properties = {p if p != props else {} for p in nouns[props.lower()]}
+                    break
+        
+
 def new_game(level_description):
     """
     Given a description of a game state, create and return a game
@@ -73,23 +135,28 @@ def new_game(level_description):
     The exact choice of representation is up to you; but note that what you
     return will be used as input to the other functions.
     """
+    print(level_description)
     board = []
-    objects = []
+    game_objs = []
     for r_idx in range(len(level_description)):
         new_row = []
         for c_idx in range(len(level_description[0])):
             if level_description[r_idx][c_idx] == []:
                 new_row.append([])
             else:
-                new_obj = gameObj(level_description[r_idx][c_idx], board, (r_idx, c_idx))
-                new_row.append(new_obj)
-                objects.append(new_obj)
+                this_level = []
+                for obj in level_description[r_idx][c_idx]:
+                    new_obj = gameObj(obj, board, (r_idx, c_idx))
+                    this_level.append(new_obj)
+                    game_objs.append(new_obj)
+                new_row.append(this_level)
         board.append(new_row)
     game = {
-        'property_map': {property: [] for property in PROPERTIES},
+        'properties': {prop:[] for prop in PROPERTIES},
         'board': board,
-        'objects': objects
+        'objects': game_objs
     }
+    are_nouns(game, find_rules(game))
     return game
 
 
@@ -103,9 +170,15 @@ def step_game(game, direction):
     step_game should return a Boolean: True if the game has been won after
     updating the state, and False otherwise.
     """
-    # if "snek" in game.values().value:
+    direction, properties, board, objects = direction_vector[direction], game['properties'], game['board'], game['objects']
+    for obj in objects:
+        print(obj.value, obj.properties)
+        if 'YOU' in obj.properties:
+            print('moving')
+            obj.move_obj(direction)
+    print(dump_game(game))
+    return False
 
-    # return False
 
 
 def dump_game(game):
@@ -118,4 +191,13 @@ def dump_game(game):
     print out the current state of your game for testing and debugging on your
     own.
     """
-    raise NotImplementedError
+    return [[[obj.value for obj in c] for c in r] for r in game['board']]
+
+curr_game = [
+      [["SNEK"], [], [], ["SNEK"]],
+      [["IS"], [], ["snek"], ["IS"]],
+      [["PULL"], ["snek"], [], ["PUSH"]],
+      [[], [], [], []]
+    ]
+
+print(dump_game(new_game(curr_game)))
